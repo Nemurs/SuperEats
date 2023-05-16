2ae36e797a4464b4fa499cc4247affd2d5195cfe
Nemurs <kevinoctaviomejia@gmail.com>
2023-05-16 15:24:20 -0700

    updated users and tested local deployment

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -6,14 +6,14 @@ from sqlalchemy.sql import text
 def seed_users():
     demo = User(
         username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+    kevin = User(
+        username='kevin', email='kevin@aa.io', password='password')
+    joe = User(
+        username='joe', email='joe@aa.io', password='password')
 
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add(kevin)
+    db.session.add(joe)
     db.session.commit()
 
 
@@ -28,5 +28,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM users"))
-        
+
     db.session.commit()
