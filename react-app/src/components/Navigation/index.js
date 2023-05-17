57e1c34274b3f2563f5fb241327aafb9bd6107d9
Nemurs <kevinoctaviomejia@gmail.com>
2023-05-16 17:17:35 -0700

    added some styling and updated layout of splash page

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -3,19 +3,37 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import OpenModalButton from "../OpenModalButton";
+import LoginFormModal from "../LoginFormModal";
+import SignupFormModal from "../SignupFormModal";
 
-function Navigation({ isLoaded }){
+function Navigation({ isLoaded }) {
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
+		<ul className='nav-list'>
+			<div className='home-sidebar-wrapper'>
+				<li className='nav-list-item'>
+					<NavLink exact to="/">Home</NavLink>
 				</li>
+			</div>
+			{isLoaded && (
+				<div className='login-signup-wrapper'>
+					<li className='nav-list-item'>
+						<OpenModalButton
+							buttonText="Log In"
+							modalComponent={<LoginFormModal />}
+							buttonClass={"white-button-round"}
+						/>
+					</li>
+					<li className='nav-list-item'>
+						<OpenModalButton
+							buttonText="Sign Up"
+							modalComponent={<SignupFormModal />}
+							buttonClass={"black-button-round"}
+						/>
+					</li>
+				</div>
 			)}
 		</ul>
 	);
