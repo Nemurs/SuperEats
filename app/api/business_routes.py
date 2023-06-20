babe3dcc2b750bd7873da575e9192cb101800cb4
Nemurs <kevinoctaviomejia@gmail.com>
2023-06-19 21:05:51 -0700

    fixed prod bug with case sensitivity in backend query

--- app/api/business_routes.py
+++ app/api/business_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, session, request
 from app.models import Business, Item, db
-from sqlalchemy import or_, and_
+from sqlalchemy import and_, func
 
 business_routes = Blueprint('business', __name__)
 
@@ -20,9 +20,10 @@ def get_some_businesses():
 
     search = list(request.args.keys())[0]
     search = search.split(" ")
-    print("search----->", search)
+    search = [term.lower() for term in search]
+    # print("search----->", search)
 
-    businesses = Business.query.join(Item).filter(and_((Business.name.like(f"%{term}%"))  | (Business.category.like(f"%{term}%")) | (Item.name.like(f"%{term}%")) | (Item.category.like(f"%{term}%")) for term in search)).all()
+    businesses = db.session.query(Business).join(Item).filter(and_(func.lower(Business.name).like(f"%{term}%")  | func.lower(Business.category).like(f"%{term}%") | func.lower(Item.name).like(f"%{term}%") | func.lower(Item.category).like(f"%{term}%") for term in search)).all()
     return {'Businesses': [bus.to_dict() for bus in businesses]}
 
 @business_routes.route('/<int:id>')
