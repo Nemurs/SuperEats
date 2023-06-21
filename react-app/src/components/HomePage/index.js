a231478ea8a8515c3f8e8a82ca71141e7b3e0e36
Nemurs <kevinoctaviomejia@gmail.com>
2023-06-21 09:14:06 -0700

    finished basic implementation of search feature

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -21,42 +21,63 @@ function HomePage() {
   if (!businesses || businesses.length !== 12) return (<Loader />)
 
   let likedBusinesses = businesses.slice()
-  likedBusinesses.sort((a,b)=> b.businessRating - a.businessRating)
+  likedBusinesses.sort((a, b) => b.businessRating - a.businessRating)
 
   let popularBusinesses = businesses.slice()
-  popularBusinesses.sort((a,b)=> Object.values(b.carts).length - Object.values(a.carts).length)
+  popularBusinesses.sort((a, b) => Object.values(b.carts).length - Object.values(a.carts).length)
 
-  const priceRedirect = (e, priceBucket) => {
+  const priceRedirect = (e, priceBucket, sortBucket) => {
     e.preventDefault()
-    history.push('search', {priceBucket})
+    if (priceBucket > 0) history.push('search', { priceBucket })
+    else history.push('search', { sortBucket })
   }
 
   return (
     <>
       <div className="home-page-wrapper">
-        <CategoryButtonIndex/>
+        <CategoryButtonIndex />
         <div className="home-page-wrapper-bottom">
           <div className="home-page-wrapper-bottom-left">
-            <h3>Sort</h3>
+            <div>
+              <h3>Sort</h3>
+              <div className="sort-button-wrapper">
+                <div>
+                  <button className="white-button-round small-padding" onClick={(e) => priceRedirect(e, 0, 'Default')}><i className="far fa-circle"/></button>
+                  <span>Default</span>
+                </div>
+                <div>
+                  <button className="white-button-round small-padding" onClick={(e) => priceRedirect(e, 0, 'Popularity')}><i className="far fa-circle"/></button>
+                  <span>Popularity</span>
+                </div>
+                <div>
+                  <button className="white-button-round small-padding" onClick={(e) => priceRedirect(e, 0, 'Rating')}><i className="far fa-circle"/></button>
+                  <span>Rating</span>
+                </div>
+                <div>
+                  <button className="white-button-round small-padding" onClick={(e) => priceRedirect(e, 0, 'Random')}><i className="far fa-circle"/></button>
+                  <span>Random</span>
+                </div>
+              </div>
+            </div>
             <div>
               <h3>Price Range</h3>
               <div className="price-button-wrapper">
-                <button className="gray-button-round small-padding" onClick={(e)=>priceRedirect(e, 1)}>$</button>
-                <button className="gray-button-round small-padding" onClick={(e)=>priceRedirect(e, 2)}>$$</button>
-                <button className="gray-button-round small-padding" onClick={(e)=>priceRedirect(e, 3)}>$$$</button>
-                <button className="gray-button-round small-padding" onClick={(e)=>priceRedirect(e, 4)}>$$$$</button>
+                <button className="gray-button-round small-padding" onClick={(e) => priceRedirect(e, 1)}>$</button>
+                <button className="gray-button-round small-padding" onClick={(e) => priceRedirect(e, 2)}>$$</button>
+                <button className="gray-button-round small-padding" onClick={(e) => priceRedirect(e, 3)}>$$$</button>
+                <button className="gray-button-round small-padding" onClick={(e) => priceRedirect(e, 4)}>$$$$</button>
               </div>
             </div>
           </div>
           <div className="home-page-wrapper-bottom-right">
             <BusinessCarousel
-              businesses={likedBusinesses.slice(0,8)}
+              businesses={likedBusinesses.slice(0, 8)}
               title={"Highly Rated"}
               tileCount={TILECOUNT}
               small={true}
             />
             <BusinessCarousel
-              businesses={popularBusinesses.slice(0,8)}
+              businesses={popularBusinesses.slice(0, 8)}
               title={"Most Popular"}
               tileCount={TILECOUNT}
               small={true}
--- react-app/src/components/SearchIndex/index.js
+++ react-app/src/components/SearchIndex/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
-import { loadSomeBusinessesThunk } from "../../store/business";
+import { loadAllBusinessesThunk, loadSomeBusinessesThunk } from "../../store/business";
 import BusinessIndex from "../BusinessIndex";
 import Loader from "../Loader";
 import "./SearchIndex.css"
@@ -10,15 +10,15 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-  }
+}
 
 function shuffle(arr) {
     let out = [];
     let visited = new Set([]);
     let max = arr.length;
-    while(out.length < max){
+    while (out.length < max) {
         let idx = getRandomInt(0, max)
-        if (!visited.has(idx)){
+        if (!visited.has(idx)) {
             visited.add(idx)
             out.push(arr[idx])
         }
@@ -29,7 +29,8 @@ function shuffle(arr) {
 const SearchIndex = () => {
     const location = useLocation();
     const dispatch = useDispatch();
-    const [sortbyOption, setSortbyOption] = useState('Default')
+    const [sortbyOption, setSortbyOption] = useState(location.state?.sortBucket ? location.state.sortBucket : 'Default')
+    const [priceOption, setPriceOption] = useState(location.state?.priceBucket ? location.state.priceBucket : 0)
     const [modifiedBusinesses, setModifiedBusinesses] = useState(null)
 
     const businesses = useSelector(state => state?.business?.allBusinesses ? Object.values(state.business.allBusinesses) : null)
@@ -37,16 +38,25 @@ const SearchIndex = () => {
     const searchTerms = location.search.slice(1);
 
     useEffect(() => {
+        if (!searchTerms) {
+            dispatch(loadAllBusinessesThunk())
+            return;
+        }
         dispatch(loadSomeBusinessesThunk(searchTerms))
     }, [dispatch])
 
     useEffect(() => {
         dispatch(loadSomeBusinessesThunk(searchTerms))
+        if (modifiedBusinesses){
+            setModifiedBusinesses(null)
+            setPriceOption(0)
+            setSortbyOption('Default')
+        }
     }, [searchTerms])
 
     useEffect(() => {
-        console.log("we are in the use effect")
-        if(!businesses) return;
+        if (!businesses) return;
+
         let busses = businesses.slice()
         switch (sortbyOption) {
             case 'Default':
@@ -54,114 +64,135 @@ const SearchIndex = () => {
                 break;
             case 'Popularity':
                 setModifiedBusinesses(prev => {
-                    if (prev){
-                        return prev.toSorted((a,b)=> Object.values(b.carts).length - Object.values(a.carts).length)
+                    if (prev) {
+                        return prev.toSorted((a, b) => Object.values(b.carts).length - Object.values(a.carts).length)
                     }
-                    return busses.toSorted((a,b)=> Object.values(b.carts).length - Object.values(a.carts).length)
+                    return busses.toSorted((a, b) => Object.values(b.carts).length - Object.values(a.carts).length)
                 })
                 break;
             case 'Rating':
                 setModifiedBusinesses(prev => {
                     console.log(prev)
-                    if (prev){
-                        let test = prev.toSorted((a,b)=> b.businessRating - a.businessRating)
+                    if (prev) {
+                        let test = prev.toSorted((a, b) => b.businessRating - a.businessRating)
                         console.log("after sorting>>>>>", test)
                         return test
                     }
-                    return busses.toSorted((a,b)=> b.businessRating - a.businessRating)
+                    return busses.toSorted((a, b) => b.businessRating - a.businessRating)
                 })
-                // busses.sort((a,b)=> b.businessRating - a.businessRating)
-                // setModifiedBusinesses(busses);
                 break;
             case 'Random':
                 setModifiedBusinesses(prev => prev ? shuffle(prev) : shuffle(busses));
                 break;
         }
-    }, [sortbyOption, searchTerms])
+    }, [sortbyOption])
 
-    useEffect(()=>{
-        if(!businesses) return;
-        switch (location.state.priceBucket) {
-            case 1:
-                setModifiedBusinesses(prev => prev ? prev.filter(bus => bus.avgItemPrice < 10) : businesses.filter(bus => bus.avgItemPrice < 10))
-                break;
-            case 2:
-                setModifiedBusinesses(prev => prev ? prev.filter(bus => bus.avgItemPrice > 10 && bus.avgItemPrice < 15) : businesses.filter(bus => bus.avgItemPrice > 10 && bus.avgItemPrice < 15))
-                break;
-            case 3:
-                setModifiedBusinesses(prev => prev ? prev.filter(bus => bus.avgItemPrice > 15 && bus.avgItemPrice < 25) : businesses.filter(bus => bus.avgItemPrice > 15 && bus.avgItemPrice < 25))
-                break;
-            case 4:
-                setModifiedBusinesses(prev => prev ? prev.filter(bus => bus.avgItemPrice > 25) : businesses.filter(bus => bus.avgItemPrice > 25))
-                break;
+    useEffect(() => {
+        if (priceOption > 0){
+            switch (priceOption) {
+                case 1:
+                    setModifiedBusinesses(prev => prev ? prev.filter(bus => bus.avgItemPrice < 10) : businesses.filter(bus => bus.avgItemPrice < 10))
+                    break;
+                case 2:
+                    setModifiedBusinesses(prev => prev ? prev.filter(bus => bus.avgItemPrice > 10 && bus.avgItemPrice < 15) : businesses.filter(bus => bus.avgItemPrice > 10 && bus.avgItemPrice < 15))
+                    break;
+                case 3:
+                    setModifiedBusinesses(prev => prev ? prev.filter(bus => bus.avgItemPrice > 15 && bus.avgItemPrice < 25) : businesses.filter(bus => bus.avgItemPrice > 15 && bus.avgItemPrice < 25))
+                    break;
+                case 4:
+                    setModifiedBusinesses(prev => prev ? prev.filter(bus => bus.avgItemPrice > 25) : businesses.filter(bus => bus.avgItemPrice > 25))
+                    break;
+            }
+        } else {
+            setModifiedBusinesses(null)
         }
-    }, [location.state.priceBucket])
 
-    if (!searchTerms && !location.state.priceBucket) return (<Loader />)
+    }, [priceOption])
 
+    if (!searchTerms && !location.state.priceBucket && !location.state.sortBucket) return (<Loader />)
+
+    const handlePriceButtonClick = (e, buttonNumber) => {
+        e.preventDefault()
+        if (buttonNumber === 1){
+            setModifiedBusinesses(null)
+            setPriceOption(prev => prev === 1 ? 0 : 1)
+        } else if (buttonNumber === 2){
+            setModifiedBusinesses(null)
+            setPriceOption(prev => prev === 2 ? 0 : 2)
+        } else if (buttonNumber === 3){
+            setModifiedBusinesses(null)
+            setPriceOption(prev => prev === 3 ? 0 : 3)
+        } else if (buttonNumber === 4){
+            setModifiedBusinesses(null)
+            setPriceOption(prev => prev === 4 ? 0 : 4)
+        }
+    }
     return (
-        <div>
-            <div>
-                <h3>Sort By</h3>
-                <div className="sortby-button-wrapper">
-                    <form>
+        <div className="search-page-wrapper">
+            <h1>Search Results</h1>
+            <div className="search-wrapper">
+                <div className="search-options">
+                    <div>
+                        <h3>Sort By</h3>
+                        <form className="sortby-form">
+                            <label>
+                                <input
+                                    type="radio"
+                                    value={'Default'}
+                                    checked={sortbyOption === 'Default'}
+                                    onChange={(e) => setSortbyOption(e.target.value)}
+                                />
+                                Default
+                            </label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    value={'Popularity'}
+                                    checked={sortbyOption === 'Popularity'}
+                                    onChange={(e) => setSortbyOption(e.target.value)}
+                                />
+                                Popularity
+                            </label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    value={'Rating'}
+                                    checked={sortbyOption === 'Rating'}
+                                    onChange={(e) => setSortbyOption(e.target.value)}
+                                />
+                                Rating
+                            </label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    value={'Random'}
+                                    checked={sortbyOption === 'Random'}
+                                    onChange={(e) => setSortbyOption(e.target.value)}
+                                />
+                                Random
+                            </label>
+                        </form>
+                    </div>
+                    <div>
+                        <h3>Price Range</h3>
+                        <div className="price-button-wrapper">
+                            <button className={`small-padding ${priceOption === 1 ? "price-button-active" : "gray-button-round"}`} onClick={(e) => handlePriceButtonClick(e,1)}>$</button>
+                            <button className={`small-padding ${priceOption === 2 ? "price-button-active" : "gray-button-round"}`} onClick={(e) => handlePriceButtonClick(e,2)}>$$</button>
+                            <button className={`small-padding ${priceOption === 3 ? "price-button-active" : "gray-button-round"}`} onClick={(e) => handlePriceButtonClick(e,3)}>$$$</button>
+                            <button className={`small-padding ${priceOption === 4 ? "price-button-active" : "gray-button-round"}`} onClick={(e) => handlePriceButtonClick(e,4)}>$$$$</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="search-index">
+                    {(searchTerms || location.state.priceBucket || location.state.sortBucket) &&
+                        (
+                            <>
 
-                        <label>
-                            <input
-                                type="radio"
-                                value={'Default'}
-                                checked={sortbyOption === 'Default'}
-                                onChange={(e) => setSortbyOption(e.target.value)}
-                            />
-                            Default
-                        </label>
-                        <label>
-                            <input
-                                type="radio"
-                                value={'Popularity'}
-                                checked={sortbyOption === 'Popularity'}
-                                onChange={(e) => setSortbyOption(e.target.value)}
-                            />
-                            Popularity
-                        </label>
-                        <label>
-                            <input
-                                type="radio"
-                                value={'Rating'}
-                                checked={sortbyOption === 'Rating'}
-                                onChange={(e) => setSortbyOption(e.target.value)}
-                            />
-                            Rating
-                        </label>
-                        <label>
-                            <input
-                                type="radio"
-                                value={'Random'}
-                                checked={sortbyOption === 'Random'}
-                                onChange={(e) => setSortbyOption(e.target.value)}
-                            />
-                            Random
-                        </label>
-                    </form>
+                                {businesses && businesses.length ? (<BusinessIndex businesses={modifiedBusinesses ? modifiedBusinesses : businesses} />) : (<p>Sorry that search had no results. Try again using a different word or phrase.</p>)}
+                                {modifiedBusinesses && !modifiedBusinesses.length && (<p>Sorry that search filter had no results.</p>)}
+                            </>
+                        )}
                 </div>
-            </div>
-            <div>
-                <h3>Price Range</h3>
-                <div className="price-button-wrapper">
-                    <button className="gray-button-round small-padding">$</button>
-                    <button className="gray-button-round small-padding">$$</button>
-                    <button className="gray-button-round small-padding">$$$</button>
-                    <button className="gray-button-round small-padding">$$$$</button>
-                </div>
-            </div>
-            <div className="search-index">
-                {(searchTerms || location.state.priceBucket) &&
-                    (
-                        <>
-                            <h1>Search Results</h1>
-                            {businesses && businesses.length ? (<BusinessIndex businesses={modifiedBusinesses ? modifiedBusinesses : businesses} />) : (<p>Sorry that search had no results. Try again using a different word or phrase.</p>)}
-                        </>
-                    )}
             </div>
 
         </div>
