0bca55e66963f978d63a5c721d3066feda46d400
Nemurs <kevinoctaviomejia@gmail.com>
2023-06-26 10:54:03 -0700

    added image upload

--- app/forms/add_image_form.py
+++ app/forms/add_image_form.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, IntegerField, BooleanField
 from wtforms.validators import DataRequired
+from app.util import ALLOWED_EXTENSIONS
 
 class AddImageForm(FlaskForm):
-    url = StringField('url', validators=[DataRequired()])
+    image = FileField("Image File", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
     preview = BooleanField('preview', validators=[DataRequired(),])
     user_id = IntegerField('user_id', validators=[DataRequired()])
--- react-app/src/components/UpdateProfilePictureModal/index.js
+++ react-app/src/components/UpdateProfilePictureModal/index.js
@@ -1,51 +1,80 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useModal } from "../../context/Modal";
 import { createProfilePicThunk, deleteProfilePicThunk } from "./profilePicThunks";
 import CloseModalButton from "../CloseModalButton";
 import "./UpdateProfilePictureModal.css";
 import { useDispatch } from "react-redux";
+import Loader from "../Loader";
 
 const UpdateProfilePictureModal = ({ imgUrl, imgId, userId }) => {
-    const {closeModal} = useModal();
+    const { closeModal } = useModal();
     const dispatch = useDispatch();
 
     const defUrl = "/default-profile-picture.png"
     const isDefPic = imgUrl === defUrl;
-    const [picUrl, setPicUrl] = useState(!isDefPic ? imgUrl : "");
+    // const [picUrl, setPicUrl] = useState(!isDefPic ? imgUrl : "");
+    const [image, setImage] = useState(null);
+    const [imageLoading, setImageLoading] = useState(false);
+    const [preview, setPreview] = useState(null)
+
+    useEffect(() => {
+        if (!image) {
+            setPreview(undefined)
+            return
+        }
+
+        const objectUrl = URL.createObjectURL(image)
+        setPreview(objectUrl)
+
+        // free memory when ever this component is unmounted
+        return () => URL.revokeObjectURL(objectUrl)
+    }, [image])
 
     if (!userId) return (<></>);
 
-    const checkUrl = (str) => {
-        return str.endsWith(".jpg") || str.endsWith(".jpeg") || str.endsWith(".png") || str.endsWith(".gif") || str.endsWith(".webp")
-    }
+    // const checkUrl = (str) => {
+    //     return str.endsWith(".jpg") || str.endsWith(".jpeg") || str.endsWith(".png") || str.endsWith(".gif") || str.endsWith(".webp")
+    // }
 
     const handleSubmit = async (e, rmPic = false) => {
         e.preventDefault();
-        if(rmPic){
-            if (!isDefPic){
+        const formData = new FormData();
+        formData.append("image", image);
+        formData.append("preview", true);
+        formData.append("user_id", userId)
+        if (rmPic) {
+            if (!isDefPic) {
                 await dispatch(deleteProfilePicThunk(userId, imgId))
             } else return;
         } else {
-            if (imgUrl === picUrl) return;
-            await dispatch(createProfilePicThunk(userId, picUrl))
+            // if (imgUrl === picUrl) return;
+            setImageLoading(true)
+            await dispatch(createProfilePicThunk(formData))
         }
+        setImageLoading(false)
         closeModal();
         return;
     }
 
     return (
         <div className="profile-pic-update-wrapper">
-            <CloseModalButton/>
+            <CloseModalButton />
             <h1>Edit Profile Picture</h1>
-            <img src={isDefPic && !checkUrl(picUrl) ? defUrl : picUrl} alt="profile picture" className="profile-pic-preview" />
-            <form onSubmit={handleSubmit} className="profile-pic-update-form">
-                <input
+            <img src={isDefPic && !imgUrl ? defUrl : image ? preview: imgUrl} alt="profile picture" className="profile-pic-preview" />
+            <form onSubmit={handleSubmit} className="profile-pic-update-form" encType="multipart/form-data">
+                {/* <input
                     type="text"
                     value={picUrl }
                     onChange={(e) => setPicUrl(e.target.value)}
                     placeholder="Enter new profile picture url"
                     required
+                /> */}
+                <input
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) => setImage(e.target.files[0])}
                 />
+                {(imageLoading) && <Loader/>}
                 <button type="submit" className="black-button-square background-green">Update Picture</button>
                 <button className="black-button-square background-red" onClick={(e) => handleSubmit(e, true)}>Delete Picture</button>
             </form>
